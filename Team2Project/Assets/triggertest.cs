47434253da0b74c9df2514b5321b4d908dbf0425
meoheee <dany1124@hanmail.net>
2021-01-27 20:19:22 +0900

    map

---
+++ Team2Project/Assets/triggertest.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class triggertest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("OnTriggerStay");
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        
+    }
+    private void OnCollisionEnter(Collider other)
+    {
+        Debug.Log("OnCollisionEnter");
+    }
+    private void OnCollisionStay(Collider other)
+    {
+
+    }
+    private void OnCollisionExit(Collider other)
+    {
+
+    }
+}
+   
